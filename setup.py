c5c3e5351c481576d8d4de80497a50a8d1986d70
rh <rh@digitronic.net>
2020-02-14 08:16:02 +0100

    update version information

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', encoding='utf-8') as f:
 #     requirements = f.read().splitlines()
 
 setuptools.setup(
-    version="1.0.1",
+    version="1.1.0",
     name="asyncrcon",
     author="zekro",
     author_email="contact@zekro.de",
